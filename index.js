5071cad32421c5eeadbceb51de5735959499c36e
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-03-27 17:09:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let result = false;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        console.log(`target: ${target}`)
+        console.log(`Sum: ${array[i] + array[j]}`)
+        result = true;
+      }
+    }
+  }
+  return result;
 }
 
 /* 
@@ -8,10 +19,31 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  hasTargetSum(array, target) {
+    for each number in the array {
+      for each element in the array {
+        if the sum of each index 2 numbers is equal to the target {
+          return true
+      }
+    }
+  }
+
+
 */
 
 /*
   Add written explanation of your solution here
+  has a target sum takes in 2 arguments, an array and a target number
+  The function will return true if any 2 numbers in the array add up to the target number
+  The function will return false if no 2 numbers in the array add up to the target number
+
+  Step 1: Create a variable called result and set it equal to false
+  Step 2: Create a for loop that will iterate through the array
+  Step 3: Create a second for loop that will iterate through the array
+  Step 4: Create an if statement that will check if the sum of the 2 numbers in the array is equal to the target number
+  Step 5: If the sum of the 2 numbers in the array is equal to the target number, set the result variable equal to true
+  Step 6: Return the result variable
+
 */
 
 // You can run `node index.js` to view these console logs
